136c55dde310b6f1a1587a63a5115c5230eee15d
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-09 14:01:52 -0500

    feat(grid): make pixels change color on mouseover
    Pixels will now change color on mouseover.
    If left click is held, the color resets

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const board = document.querySelector('.container');
+const board = document.querySelector('.board');
 let pixels = [];
-let sideLength = 5;
+let sideLength = 16;
 
-for (i = 0; i < sideLength; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    for (j = 0; j < sideLength; j++) {
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        row.appendChild(pixel);
-    }
-    board.appendChild(row);
+function makegrid(n) {
+    for (i = 0; i < n; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (j = 0; j < n; j++) {
+            let pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mouseover', event => {mouseover(event)});
+            row.appendChild(pixel);
+        }
+        board.appendChild(row);
+    }    
 }
+
+function mouseover(e) {
+    if (e.buttons == 1) {
+        e.target.style.backgroundColor = 'white';
+    }
+    else {
+        e.target.style.backgroundColor = 'black';
+    }
+}
+
+makegrid(sideLength);
